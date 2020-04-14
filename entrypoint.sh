ce1c73ff6d523fc2df01e9a98b60395abb1caf98
niyarin <inoanet@gmail.com>
2020-04-15 08:43:47 +0900

    Add argument for runner.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 cd /lint-action-clj
-clojure -m lint-action "{:linters $1 :cwd \"/github/workspace/\" :mode :github-action :relative-dir $2  :file-target :git}"
+clojure -m lint-action "{:linters $1 :cwd \"/github/workspace/\" :mode :github-action :relative-dir $2  :file-target :git :runner $3}"
