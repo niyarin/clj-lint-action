036f4964c737188a8c9dae9291bfac2d470d4f42
niyarin <inoanet@gmail.com>
2020-05-05 22:04:14 +0900

    Change action for calculationg diff files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /lint-action-clj
 
-clojure -m lint-action "{:linters $1 :cwd \"${GITHUB_WORKSPACE}\" :mode :github-action :relative-dir $2  :file-target :git :runner $3 :git-sha \"${GITHUB_SHA}\" :use-files $4 :files $5}"
+clojure -m lint-action "{:linters $1 :cwd \"${GITHUB_WORKSPACE}\" :mode :github-action :relative-dir $2  :file-target :git :runner $3 :git-sha \"${GITHUB_SHA}\" :use-files $4 :files [$5]}"
