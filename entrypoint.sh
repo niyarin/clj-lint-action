85f5ddb76911d2a67e26870e49dd3d904e43f14e
niyarin <inoanet@gmail.com>
2020-05-16 04:11:36 +0900

    fix argument bag

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ FILES=`git diff HEAD FETCH_HEAD --name-only|grep '\.clj$'|sed 's/^.*$/"&"/g'|tr 
 
 cd /lint-action-clj
 
-FILES=`echo $5|sed "s/'/\\"/g"`
+
+echo  "{:linters $1 :cwd \"${GITHUB_WORKSPACE}\" :mode :github-action :relative-dir $2  :file-target :git :runner $3 :git-sha \"${GITHUB_SHA}\" :use-files true :files [$FILES] :eastwood-linters $5}"
+
 clojure -m lint-action "{:linters $1 :cwd \"${GITHUB_WORKSPACE}\" :mode :github-action :relative-dir $2  :file-target :git :runner $3 :git-sha \"${GITHUB_SHA}\" :use-files true :files [$FILES] :eastwood-linters $5}"
